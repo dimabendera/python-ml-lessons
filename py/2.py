59b94150e609ff8ed37079b4b70d32f3a1e19122
dmitroprobachay <dmytro.probachay@ria.com>
2023-10-17 08:29:01 +0300

    Added py

---
+++ py/2.py
@@ -0,0 +1,12 @@
+"""
+1) В нас є строчка з нікнеймом "@dada" за допомогою slice вивести ту ж стрічку
+   без собачки та з великої першої букви, щоб всі великі та всі маленькі
+   (capitalize, upper, lower)
+2) Тепер змінимо строку на "Hello my nickname is @dada in Twitter"
+   Нам треба так само взяти тільки "dada" з неї та вивести в трьох варінтах.
+   (find і slice)
+3) В нас є строчка "Hello my nickname is @dada in Twitter and @daat in Instagram and in Twitter and @daat in Instagram"
+   Треба розпарсити всі входження ніків за допомогою цикла while
+"""
+
+
